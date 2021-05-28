a1db3377408be0135b8d750a0d7ddcfe2ed2e4fd
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-05-28 15:51:24 +0800

    [init]

---
+++ pipeline.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Find Zh-Wiki Here: https://dumps.wikimedia.org/zhwiki/
+## curl -O https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2 
+
+## Get Wiki Corpus And Transform ZhTW
+WIKI_INPUT_PATH="/Users/allenyummy/Documents/Word2Vec/zhwiki-latest-pages-articles.xml.bz2"
+WIKI_OUTPUT_PATH="/Users/allenyummy/Documents/Word2Vec/data/processed/wiki_corpus.txt"
+
+python wiki2txt.py \
+    -i $WIKI_INPUT_PATH \
+    -o $WIKI_OUTPUT_PATH \
---
+++ wiki2txt.py
@@ -0,0 +1,55 @@
+# encoding=utf-8
+# Author: Yu-Lun Chiang
+# Description: Get Wiki Corpus and Transform into ZhTW
+
+import logging
+import os
+import argparse
+import opencc
+from gensim.corpora import WikiCorpus
+
+logger = logging.getLogger(__name__)
+converter = opencc.OpenCC("s2twp.json")
+
+
+def main():
+
+    """ Args """
+    parser = argparse.ArgumentParser(description="Extract Wiki Corpus To File.")
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        help="input file path for wiki corpus",
+        dest="INPUT",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="output txt file path",
+        dest="OUTPUT",
+    )
+    args = parser.parse_args()
+
+    """ Create Output Directory """
+    if not os.path.exists(os.path.dirname(args.OUTPUT)):
+        os.makedirs(os.path.dirname(args.OUTPUT))
+
+    """ Extract AND Transform zhtw"""
+    wiki_corpus = WikiCorpus(fname=args.INPUT, dictionary={})
+    count = 0
+    with open(args.OUTPUT, "w", encoding="utf-8") as output:
+        for text in wiki_corpus.get_texts():
+
+            orig_text = " ".join(text) + "\n"
+            zhtw_text = converter.convert(orig_text)
+            output.write(zhtw_text)
+            count += 1
+
+            if count % 10000 == 0:
+                logging.info(f"It has already {count} 篇文章")
+
+
+if __name__ == "__main__":
+    main()
