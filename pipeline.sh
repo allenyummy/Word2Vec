3a48462a7274bfc7a498f836498a765dea9bc20d
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-06-02 13:01:01 +0800

    [modified] train word2vec model

--- pipeline.sh
+++ pipeline.sh
@@ -4,12 +4,36 @@
 ## curl -O https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2 
 
 ## Get Wiki Corpus And Transform ZhTW
-WIKI_INPUT_PATH="/Users/allenyummy/Documents/Word2Vec/data/zhwiki-latest-pages-articles.xml.bz2"
-WIKI_OUTPUT_PATH="/Users/allenyummy/Documents/Word2Vec/data/processed/wiki_corpus.txt"
+echo ===== Proprocess Corpus =====
+
+WIKI_INPUT_PATH=data/zhwiki-latest-pages-articles.xml.bz2
+WIKI_OUTPUT_PATH=data/processed/wiki_corpus_5.txt
+GPU_INDEX=-1  ## -1 means cpu
+SAVE_STEPS=5000
+DEBUG=True
 
 python wiki2txt.py \
-    -i $WIKI_INPUT_PATH \
-    -o $WIKI_OUTPUT_PATH \
+    --input $WIKI_INPUT_PATH \
+    --output $WIKI_OUTPUT_PATH \
+    --gpu_index $GPU_INDEX \
+    --save_steps $SAVE_STEPS \
+    --debug $DEBUG \
+
+
+## Train Word2Vec model
+echo ===== Train Word2Vec Model =====
+
+TODAY=$(date +"%Y%m%d%H%M%S")
+VECTOR_SIZE=250
+MIN_COUNT=5
+EPOCHS=5
+SEED=1
+TRAINED_MODEL_PATH=model/word2vec_${TODAY}_v${VECTOR_SIZE}_c${MIN_COUNT}_e${EPOCHS}_s${SEED}.model
 
 python train.py \
-    -i $WIKI_OUTPUT_PATH \
+    --input $WIKI_OUTPUT_PATH \
+    --output $TRAINED_MODEL_PATH \
+    --vector_size $VECTOR_SIZE \
+    --min_count $MIN_COUNT \
+    --epochs $EPOCHS \
+    --seed $SEED \
--- train.py
+++ train.py
@@ -3,6 +3,7 @@
 # Description: Train own Word2Vec model
 
 import logging
+import os
 import argparse
 from gensim.models import word2vec
 
@@ -13,19 +14,40 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description="Train Word2Vec Model")
     parser.add_argument(
-        "-i",
         "--input",
         type=str,
         help="input file path for wiki corpus",
-        dest="INPUT",
     )
     parser.add_argument(
-        "-o",
         "--output",
         type=str,
-        help="output txt file path",
-        dest="OUTPUT",
+        help="output model path",
     )
+    parser.add_argument(
+        "--vector_size",
+        type=int,
+        default=250,
+        help="Dimensionality of the word vectors.",
+    )
+    parser.add_argument(
+        "--min_count",
+        type=int,
+        default=5,
+        help="Ignores all words with total frequency lower than this."
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=5,
+        help="Number of iterations (epochs) over the corpus."
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=1,
+        help="Initial vectors for each word are seeded with a hash of the concatenation of word + str(seed)."
+    )
+
     args = parser.parse_args()
     return args
 
@@ -35,14 +57,18 @@ def main():
     """ Args """
     args = parse_args()
 
+    """ Create Output Directory """
+    if not os.path.exists(os.path.dirname(args.output)):
+        os.makedirs(os.path.dirname(args.output))
+
     """ Load Corpus """
-    sentences = word2vec.LineSentence(args.INPUT)
+    sentences = word2vec.LineSentence(args.input)
 
     """ Train """
-    model = word2vec.Word2Vec(sentences, vector_size=250, min_count=10)
+    model = word2vec.Word2Vec(sentences, vector_size=args.vector_size, min_count=args.min_count, seed=args.seed, epochs=args.epochs)
 
     """ Save """
-    model.save("word2vec.model")
+    model.save(args.output)
 
 
 if __name__ == "__main__":
