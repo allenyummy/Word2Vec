5f09614f7afcbacac8d3976284768d5114e0dc31
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-06-02 13:01:20 +0800

    [modified] parser

--- wiki2txt.py
+++ wiki2txt.py
@@ -11,26 +11,38 @@ from gensim.corpora import WikiCorpus
 from ckip_transformers.nlp import CkipWordSegmenter
 
 logger = logging.getLogger(__name__)
-converter = opencc.OpenCC("s2twp.json")
-ws_driver = CkipWordSegmenter(level=3)
 
 
 def parse_args():
 
     parser = argparse.ArgumentParser(description="Extract Wiki Corpus To File.")
     parser.add_argument(
-        "-i",
         "--input",
         type=str,
         help="input file path for wiki corpus",
-        dest="INPUT",
     )
     parser.add_argument(
-        "-o",
         "--output",
         type=str,
         help="output txt file path",
-        dest="OUTPUT",
+    )
+    parser.add_argument(
+        "--gpu_index",
+        type=int,
+        default=-1,
+        help="Setting this to -1 will leverage CPU, a positive will run the model on the associated CUDA device id.",
+    )
+    parser.add_argument(
+        "--save_steps",
+        type=int,
+        default=5000,
+        help="save to file every n steps",
+    )
+    parser.add_argument(
+        "--debug",
+        type=bool,
+        default=False,
+        help="debug mode",
     )
     args = parser.parse_args()
     return args
@@ -39,19 +51,32 @@ def parse_args():
 def main():
 
     """ Args """
+    logger.info(" === PARSE ARGS === ")
     args = parse_args()
+    logger.info(args)
+
+    """ Language Converter """
+    converter = opencc.OpenCC("s2twp.json")
+
+    """ Tokenizer """
+    tokenizer = CkipWordSegmenter(level=3, device=args.gpu_index)
 
     """ Create Output Directory """
-    if not os.path.exists(os.path.dirname(args.OUTPUT)):
-        os.makedirs(os.path.dirname(args.OUTPUT))
+    if not os.path.exists(os.path.dirname(args.output)):
+        os.makedirs(os.path.dirname(args.output))
 
     """ Preprocess """
+    logger.info(" === Preprocess === ")
     ## Extract wiki corpus
-    wiki_corpus = WikiCorpus(fname=args.INPUT, dictionary={})
+    wiki_corpus = WikiCorpus(fname=args.input, dictionary={})
     count = 0
     tmp_articles = list()
-    with open(args.OUTPUT, "w", encoding="utf-8") as output:
+    with open(args.output, "w", encoding="utf-8") as output:
         for text in tqdm(wiki_corpus.get_texts()):
+
+            ## SAVE 5 articles for debug mode
+            if args.debug and count >= 5:
+                break
 
             orig_text = " ".join(text)
 
@@ -60,16 +85,16 @@ def main():
             tmp_articles.append(zhtw_text)
             count += 1
 
-            ## Every 10000 articles, tokenize them once.
-            if count % 10000 == 0:
-                tokenized_articles = ws_driver(tmp_articles)
+            ## Every args.SAVE_STEPS articles, tokenize them once and save them.
+            if count % args.save_steps == 0:
+                tokenized_articles = tokenizer(tmp_articles)
                 for tokenized_zhtw_text in tokenized_articles:
                     tmp_text = " ".join(tokenized_zhtw_text) + "\n"
                     output.write(tmp_text)
                 tmp_articles = list()
 
         if tmp_articles:
-            tokenized_articles = ws_driver(tmp_articles)
+            tokenized_articles = tokenizer(tmp_articles)
             for tokenized_zhtw_text in tokenized_articles:
                 tmp_text = " ".join(tokenized_zhtw_text) + "\n"
                 output.write(tmp_text)
