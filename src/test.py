a2d19b81798079f3ede1a1f4b9d4e393766610f9
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-06-10 12:45:35 +0800

    [add] logging

--- src/test.py
+++ src/test.py
@@ -3,11 +3,14 @@
 # Description: Inference
 
 import logging
+import json
 from typing import List, Tuple
 from gensim.models import word2vec
 from gensim.models import KeyedVectors
 
+logging.basicConfig()
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def load_model(use_fast: bool):
@@ -33,10 +36,11 @@ def infer(wv, word: str, topn: int = 10) -> List[Tuple[str, float]]:
 
 if __name__ == "__main__":
 
+    logger.info("Load model ...")
     wv = load_model(use_fast=True)
 
+    logger.info("Infer ...")
     ret = dict()
-
     with open(
         "/Users/allenyummy/Documents/news_classifier/src/utils/keywords/negative_news/NN_keywords.txt",
         "r",
@@ -51,6 +55,6 @@ if __name__ == "__main__":
                 res = infer(wv, word, topn=10)
                 ret[word] = res
 
-    import json
+    logger.info("Writing file ...")
     with open("nn.json", "w", encoding="utf-8") as fo:
         json.dump(ret, fo, ensure_ascii=False, indent=4)
